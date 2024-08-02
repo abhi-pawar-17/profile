85c908e50b92e42aa822be1d2a75b8ecb80796d8
abhi-pawar-17 <aspawar1727@gmail.com>
2024-08-02 21:58:48 +0530

    Update Gallary.jsx

--- src/components/Gallary.jsx
+++ src/components/Gallary.jsx
@@ -1,9 +1,7 @@
 import React from "react";
-import profileImage1 from "../assets/126746.jpg"; // Adjust the path as needed
-
+import profileImage2 from "../assets/download.jpg"; // Adjust the path as needed
 
 const Gallery = () => {
-
   const handleClick2 = () => {
     window.open(
       "https://abhi-pawar-17.github.io/zom-clone/",
@@ -19,11 +17,10 @@ const Gallery = () => {
           Project Gallery
         </h1>
         <div className="flex flex-wrap justify-center gap-8">
-       
           <ProjectCard
-            imageSrc={profileImage1}
-            title="Zomato Clone "
-            description="This is my static zomato Clone."
+            imageSrc={profileImage2}
+            title="Zomato Clone"
+            description="This is my static Zomato Clone."
             onClick={handleClick2}
           />
         </div>
@@ -34,14 +31,18 @@ const Gallery = () => {
 
 const ProjectCard = ({ imageSrc, title, description, onClick }) => {
   return (
-    <div className="relative bg-black rounded-lg overflow-hidden shadow-lg transition-transform transform hover:scale-105 hover:shadow-2xl w-80 h-80">
-      <img src={imageSrc} alt={title} className="w-full h-2/3 object-cover" />
-      <div className="p-4 bg-gradient-to-t from-gray-900 via-gray-800 to-transparent absolute bottom-0 w-full h-1/3 flex flex-col justify-end">
-        <h2 className="text-xl font-bold text-white mb-2">{title}</h2>
-        <p className="text-gray-300 mb-4">{description}</p>
+    <div className="bg-black rounded-lg overflow-hidden shadow-lg transition-transform transform hover:scale-105 hover:shadow-2xl w-80 h-80 flex flex-col">
+      <div className="w-full h-[600px] ">
+        <img src={imageSrc} alt={title} className="w-full h-full object-cover" />
+      </div>
+      <div className="p-4 bg-gray-500 w-full h-[600px]  flex flex-col justify-between">
+        <div>
+          <h2 className="text-xl font-bold text-white">{title}</h2>
+          <p className="text-gray-300">{description}</p>
+        </div>
         <button
           onClick={onClick}
-          className="px-4 py-2 bg-red-600 text-white rounded-lg shadow-md hover:bg-red-700 transition-transform duration-300"
+          className="mt-4 px-4 py-2 bg-red-600 text-white rounded-lg shadow-md hover:bg-red-700 transition-transform duration-300"
         >
           View Project
         </button>
